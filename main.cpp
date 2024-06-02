c248166765c9a1ef1f8ebe708418c510bde0edfd
Base <nikitoskey777gmail.com>
2024-06-02 18:22:21 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+    std::string Name;
+    std::cout << "Enter the Name\n";
+    std::cin >> Name;
+    std::cout <<"Your Name: " << Name << std::endl;
+    return 0;
+}
